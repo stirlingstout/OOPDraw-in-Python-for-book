32797a49b526e63e7f3865e79b5fe8fa80ba7ba1
John Stirling Stout <cuspcomputers@outlook.com>
2020-09-14 11:38:52 +0100

    Exercise 10

--- OOPDraw/shape.py
+++ OOPDraw/shape.py
@@ -6,10 +6,32 @@ class Shape(ABC):
     will be used in OOPDraw. I can't be instantiated
     myself since I'm abstract"""
 
+    def __init__(self, p: wx.Pen, x1: int, y1: int, x2: int=None, y2: int=None):
+        self.__Pen = p
+        self.__X1 = x1
+        self.__Y1 = y1
+        self.__X2 = x2 if x2 else x1
+        self.__Y2 = y2 if y2 else y1
+
+    def Pen(self):
+        return self.__Pen
+
+    def X1(self):
+        return self.__X1
+
+    def Y1(self):
+        return self.__Y1
+
+    def X2(self):
+        return self.__X2
+
+    def Y2(self):
+        return self.__Y2
+      
     @abstractmethod
     def Draw(self, dc: wx.DC):
         pass
 
-    @abstractmethod
     def GrowTo(self, x2: int, y2: int):
-        pass
+        self.__X2 = x2
+        self.__Y2 = y2
--- OOPDraw/shapes.py
+++ OOPDraw/shapes.py
@@ -6,6 +6,34 @@ class Shape(ABC):
     will be used in OOPDraw. I can't be instantiated
     myself since I'm abstract"""
 
+    def __init__(self, p: wx.Pen, x1: int, y1: int, x2: int=None, y2: int=None):
+        self.__Pen = p
+        self.__X1 = x1
+        self.__Y1 = y1
+        self.__X2 = x2 if x2 else x1
+        self.__Y2 = y2 if y2 else y1
+
+    def Pen(self):
+        return self.__Pen
+
+    def X1(self):
+        return self.__X1
+
+    def Y1(self):
+        return self.__Y1
+
+    def X2(self):
+        return self.__X2
+
+    def Y2(self):
+        return self.__Y2
+
+    def SetX2(self, value: int):
+        self.__X2 = value
+
+    def SetY2(self, value: int):
+        self.__Y2 = value
+
     @abstractmethod
     def Draw(self, dc: wx.DC):
         pass
@@ -21,35 +49,13 @@ the (x, y) coordinates of my start and end points, and the pen
 that I am to be drawn in
 """
 
-    def __init__(self, p: wx.Pen, x1: int, y1: int, x2: int=None, y2: int=None):
-        self.__Pen = p
-        self.__X1 = x1
-        self.__Y1 = y1
-        self.__X2 = x2 if x2 else x1
-        self.__Y2 = y2 if y2 else y1
-
-    def Pen(self):
-        return self.__Pen
-
-    def X1(self):
-        return self.__X1
-
-    def Y1(self):
-        return self.__Y1
-
-    def X2(self):
-        return self.__X2
-
-    def Y2(self):
-        return self.__Y2
-      
     def Draw(self, dc: wx.DC):
-        dc.SetPen(self.__Pen)
-        dc.DrawLine(self.__X1, self.__Y1, self.__X2, self.__Y2)
+        dc.SetPen(self.Pen())
+        dc.DrawLine(self.X1(), self.Y1(), self.X2(), self.Y2())
  
     def GrowTo(self, x2: int, y2: int):
-        self.__X2 = x2
-        self.__Y2 = y2
+        self.SetX2(x2)
+        self.SetY2(y2)
 
 
 class Rectangle(Shape):
@@ -57,29 +63,7 @@ class Rectangle(Shape):
     Pen, and the start and end points of my top left and bottom 
     corners of the rectangle.
     My size can be changed using my GrowTo method"""
-
-    def __init__(self, p: wx.Pen, x1: int, y1: int, x2: int=None, y2: int=None):
-        self.__Pen = p
-        self.__X1 = x1
-        self.__Y1 = y1
-        self.__X2 = x2 if x2 else x1
-        self.__Y2 = y2 if y2 else y1
-        
-    def Pen(self):
-        return self.__Pen
-
-    def X1(self):
-        return self.__X1
-
-    def Y1(self):
-        return self.__Y1
-
-    def X2(self):
-        return self.__X2
-
-    def Y2(self):
-        return self.__Y2
-      
+ 
     def Draw(self, dc: wx.DC):
         dc.SetPen(self.__Pen) 
         x = min([self.__X1, self.__X2])
@@ -89,8 +73,8 @@ class Rectangle(Shape):
         dc.DrawRectangle(x, y, w, h)
  
     def GrowTo(self, x2: int, y2: int):
-        self.__X2 = x2
-        self.__Y2 = y2
+        self.SetX2(x2)
+        self.SetY2(y2)
 
 
 
